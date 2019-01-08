23c134656aff2fcee4836ba147408faf073268a3
MaheshKalyan <34441574+MaheshKalyan@users.noreply.github.com>
2019-01-08 15:58:33 +0530

    Add files via upload

---
+++ iris_ML.py
@@ -0,0 +1,220 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "../input"]).decode("utf8"))
+
+#load the dataset
+iris = pd.read_csv("../input/Iris.csv") 
+
+#show the first 2 rows from the dataset
+iris.head(2) 
+
+#checking if there is any inconsistency in the dataset
+#as we see there are no null values in the dataset, so the data can be processed
+
+iris.info()  
+
+#Removing the unneeded column
+#dropping the Id column as it is unecessary, axis=1 specifies that it should be column wise, inplace =1 means the changes should be reflected into the dataframe
+iris.drop('Id',axis=1,inplace=True) 
+
+
+#Some Exploratory Data Analysis With Iris
+
+#The above graph shows relationship between the sepal length and width.
+fig = iris[iris.Species=='Iris-setosa'].plot(kind='scatter',x='SepalLengthCm',y='SepalWidthCm',color='orange', label='Setosa')
+iris[iris.Species=='Iris-versicolor'].plot(kind='scatter',x='SepalLengthCm',y='SepalWidthCm',color='blue', label='versicolor',ax=fig)
+iris[iris.Species=='Iris-virginica'].plot(kind='scatter',x='SepalLengthCm',y='SepalWidthCm',color='green', label='virginica', ax=fig)
+fig.set_xlabel("Sepal Length")
+fig.set_ylabel("Sepal Width")
+fig.set_title("Sepal Length VS Width")
+fig=plt.gcf()
+fig.set_size_inches(10,6)
+plt.show()
+
+
+#Now we will check relationship between the petal length and width.
+
+fig = iris[iris.Species=='Iris-setosa'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='orange', label='Setosa')
+iris[iris.Species=='Iris-versicolor'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='blue', label='versicolor',ax=fig)
+iris[iris.Species=='Iris-virginica'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='green', label='virginica', ax=fig)
+fig.set_xlabel("Petal Length")
+fig.set_ylabel("Petal Width")
+fig.set_title(" Petal Length VS Width")
+fig=plt.gcf()
+fig.set_size_inches(10,6)
+plt.show()
+
+
+#Now let us see how are the length and width are distributed
+iris.hist(edgecolor='black', linewidth=1.2)
+fig=plt.gcf()
+fig.set_size_inches(12,6)
+plt.show()
+
+#Now let us see how the length and width vary according to the species
+plt.figure(figsize=(15,10))
+plt.subplot(2,2,1)
+sns.violinplot(x='Species',y='PetalLengthCm',data=iris)
+plt.subplot(2,2,2)
+sns.violinplot(x='Species',y='PetalWidthCm',data=iris)
+plt.subplot(2,2,3)
+sns.violinplot(x='Species',y='SepalLengthCm',data=iris)
+plt.subplot(2,2,4)
+sns.violinplot(x='Species',y='SepalWidthCm',data=iris)
+
+
+
+# importing alll the necessary packages to use the various classification algorithms
+from sklearn.linear_model import LogisticRegression  	# for Logistic Regression algorithm
+from sklearn.cross_validation import train_test_split 	#to split the dataset for training and testing
+from sklearn.neighbors import KNeighborsClassifier  	# for K nearest neighbours
+from sklearn import svm  #for Support Vector Machine (SVM) Algorithm
+from sklearn import metrics 	#for checking the model accuracy
+from sklearn.tree import DecisionTreeClassifier  	#for using Decision Tree Algoithm
+
+#get the shape of the dataset
+iris.shape 
+
+#Correlation
+plt.figure(figsize=(7,4)) 
+sns.heatmap(iris.corr(),annot=True,cmap='cubehelix_r') #draws  heatmap with input as the correlation matrix calculted by(iris.corr())
+plt.show()
+
+
+#Splitting The Data into Training And Testing Dataset
+train, test = train_test_split(iris, test_size = 0.3)# in this our main data is split into train and test
+# the attribute test_size=0.3 splits the data into 70% and 30% ratio. train=70% and test=30%
+print(train.shape)
+print(test.shape)
+
+# taking the training data features
+train_X = train[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']]
+
+# output of our training data
+train_y=train.Species
+
+# taking test data features
+test_X= test[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']] 
+
+#output value of test data
+test_y =test.Species
+
+#Lets check the Train and Test Dataset
+train_X.head(2)
+test_X.head(2)
+
+##output of the training data
+train_y.head()  
+
+
+#Support Vector Machine (SVM)
+model = svm.SVC() #select the algorithm
+model.fit(train_X,train_y) # we train the algorithm with the training data and the training output
+prediction=model.predict(test_X) #now we pass the testing data to the trained algorithm
+print('The accuracy of the SVM is:',metrics.accuracy_score(prediction,test_y))#now we check the accuracy of the algorithm. 
+#we pass the predicted output by the model and the actual output
+
+
+#Logistic Regression
+model = LogisticRegression()
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the Logistic Regression is',metrics.accuracy_score(prediction,test_y))
+
+#Decision Tree
+model=DecisionTreeClassifier()
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the Decision Tree is',metrics.accuracy_score(prediction,test_y))
+
+
+#K-Nearest Neighbours
+model=KNeighborsClassifier(n_neighbors=3) #this examines 3 neighbours for putting the new data into a class
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the KNN is',metrics.accuracy_score(prediction,test_y))
+
+
+#Let's check the accuracy for various values of n for K-Nearest nerighbours¶
+a_index=list(range(1,11))
+a=pd.Series()
+x=[1,2,3,4,5,6,7,8,9,10]
+for i in list(range(1,11)):
+    model=KNeighborsClassifier(n_neighbors=i) 
+    model.fit(train_X,train_y)
+    prediction=model.predict(test_X)
+    a=a.append(pd.Series(metrics.accuracy_score(prediction,test_y)))
+plt.plot(a_index, a)
+plt.xticks(x)
+
+
+petal=iris[['PetalLengthCm','PetalWidthCm','Species']]
+sepal=iris[['SepalLengthCm','SepalWidthCm','Species']]
+train_p,test_p=train_test_split(petal,test_size=0.3,random_state=0)  #petals
+train_x_p=train_p[['PetalWidthCm','PetalLengthCm']]
+train_y_p=train_p.Species
+test_x_p=test_p[['PetalWidthCm','PetalLengthCm']]
+test_y_p=test_p.Species
+
+#We used all the features of iris in above models. Now we will use Petals and Sepals Seperately¶
+#Creating Petals And Sepals Training Data
+train_s,test_s=train_test_split(sepal,test_size=0.3,random_state=0)  #Sepal
+train_x_s=train_s[['SepalWidthCm','SepalLengthCm']]
+train_y_s=train_s.Species
+test_x_s=test_s[['SepalWidthCm','SepalLengthCm']]
+test_y_s=test_s.Species
+
+
+#SVM
+model=svm.SVC()
+model.fit(train_x_p,train_y_p) 
+prediction=model.predict(test_x_p) 
+print('The accuracy of the SVM using Petals is:',metrics.accuracy_score(prediction,test_y_p))
+
+model=svm.SVC()
+model.fit(train_x_s,train_y_s) 
+prediction=model.predict(test_x_s) 
+print('The accuracy of the SVM using Sepal is:',metrics.accuracy_score(prediction,test_y_s))
+
+#Logistic Regression
+model = LogisticRegression()
+model.fit(train_x_p,train_y_p) 
+prediction=model.predict(test_x_p) 
+print('The accuracy of the Logistic Regression using Petals is:',metrics.accuracy_score(prediction,test_y_p))
+
+model.fit(train_x_s,train_y_s) 
+prediction=model.predict(test_x_s) 
+print('The accuracy of the Logistic Regression using Sepals is:',metrics.accuracy_score(prediction,test_y_s))
+
+#Decision Tree
+model=DecisionTreeClassifier()
+model.fit(train_x_p,train_y_p) 
+prediction=model.predict(test_x_p) 
+print('The accuracy of the Decision Tree using Petals is:',metrics.accuracy_score(prediction,test_y_p))
+
+model.fit(train_x_s,train_y_s) 
+prediction=model.predict(test_x_s) 
+print('The accuracy of the Decision Tree using Sepals is:',metrics.accuracy_score(prediction,test_y_s))
+
+#K-Nearest Neighbours
+model=KNeighborsClassifier(n_neighbors=3) 
+model.fit(train_x_p,train_y_p) 
+prediction=model.predict(test_x_p) 
+print('The accuracy of the KNN using Petals is:',metrics.accuracy_score(prediction,test_y_p))
+
+model.fit(train_x_s,train_y_s) 
+prediction=model.predict(test_x_s) 
+print('The accuracy of the KNN using Sepals is:',metrics.accuracy_score(prediction,test_y_s)
+
+
+
